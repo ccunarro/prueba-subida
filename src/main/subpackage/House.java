170548feba7904f971ec2721cee6df8e7c029bbc
Carlos <carloscunarro@gmail.com>
2017-12-15 12:45:21 +0000

    Updating translations for src/main/subpackage/House.java

---
+++ src/main/subpackage/House.java
@@ -0,0 +1,25 @@
+package main.subpackage;
+
+
+//clase creada solamente para probar un commit en github
+public class House {
+
+    String address;
+    String size;
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getSize() {
+        return size;
+    }
+
+    public void setSize(String size) {
+        this.size = size;
+    }
+}
